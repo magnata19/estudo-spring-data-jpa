c9fb9e5bd5016e9317ad8174fadcccb59e801170
Davidson <dehpacifico@gmail.com>
2024-02-06 21:30:06 -0300

    criar metodos de atualizar, buscar e deletar cliente e manipular e efetuar as operacoes(crud)

--- src/main/java/io/com/pacifico/JavaApplication/JavaApplication.java
+++ src/main/java/io/com/pacifico/JavaApplication/JavaApplication.java
@@ -16,11 +16,34 @@ public class JavaApplication {
 	@Bean
 	public CommandLineRunner exibir(Clientes clientes) {
 		return args -> {
+			System.out.println("Salvando Cliente");
 			clientes.salvarCliente(new Cliente("Davidson"));
 			clientes.salvarCliente(new Cliente("Pacifico"));
 
 			List<Cliente> listarClientes = clientes.listarClientes();
 			listarClientes.forEach(System.out::println);
+
+			System.out.println("Atualizando Cliente");
+			listarClientes.forEach(c -> {
+				c.setNome(c.getNome() + "atualizado.");
+				clientes.atualizarCliente(c);
+				System.out.println(c);
+			});
+
+			System.out.println("Buscando Clientes ");
+			clientes.buscarPorNome("atual").forEach(System.out::println);
+
+//			System.out.println("Deletando clientes");
+//			clientes.listarClientes().forEach(c -> {
+//				clientes.deletar(c);
+//			});
+
+			listarClientes = clientes.listarClientes();
+			if(listarClientes.isEmpty()) {
+				System.out.println("Nenhum cliente encontrado.");
+			} else {
+				listarClientes.forEach(System.out::println);
+			}
 		};
 	}
 
