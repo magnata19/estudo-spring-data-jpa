1d8b93b799c2e423706998afc8dbe69357fc18a5
Davidson <dehpacifico@gmail.com>
2024-02-20 22:25:25 -0300

    adicionar spring security ao API

--- src/main/java/io/com/pacifico/JavaApplication/rest/controller/ClienteController.java
+++ src/main/java/io/com/pacifico/JavaApplication/rest/controller/ClienteController.java
@@ -7,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/clientes")
