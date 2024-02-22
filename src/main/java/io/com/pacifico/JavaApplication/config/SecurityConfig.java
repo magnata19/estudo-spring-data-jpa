477dd882868b181b47293bb305fb20181f10acfb
Davidson <dehpacifico@gmail.com>
2024-02-21 22:29:38 -0300

    configurar spring security

--- src/main/java/io/com/pacifico/JavaApplication/config/SecurityConfig.java
+++ src/main/java/io/com/pacifico/JavaApplication/config/SecurityConfig.java
@@ -1,30 +1,43 @@
 package io.com.pacifico.JavaApplication.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeHttpRequests((authz) -> authz
-                    .anyRequest().authenticated()
-            )
-            .httpBasic(withDefaults());
-    return http.build();
-  }
+public class SecurityConfig{
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
+    return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    UserDetails user = User.builder()
+            .username("david")
+            .password(passwordEncoder().encode("123"))
+            .roles("USER")
+            .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+
 }
+ 
